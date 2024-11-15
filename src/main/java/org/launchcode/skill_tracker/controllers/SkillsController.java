27906b0c7bcd3044f36e49b5a43e3809fa1569a9
Angie <angiepolitte@gmail.com>
2024-11-15 08:05:26 -0600

    Two Display Button Choices

--- src/main/java/org/launchcode/skill_tracker/controllers/SkillsController.java
+++ src/main/java/org/launchcode/skill_tracker/controllers/SkillsController.java
@@ -11,7 +11,7 @@ public class SkillsController {
     @GetMapping("form")
     @ResponseBody
     public String helloForm() {
-        return "<form method ='post' action = '/table'>" +
+        return "<form method ='post' action = '/display'>" +
                 "<p>Name<br/><input type = 'text' name = 'name' /></p>" +
                 "<p>My favorite language:<br/><select name = 'lang1'>" +
                 "<option value = 'Java'>Java</option>" +
@@ -28,30 +28,43 @@ public class SkillsController {
                 "<option value = 'JavaScript'>JavaScript</option>" +
                 "<option value = 'Python'>Python</option>" +
                 "</select></p>" +
-                "<input type = 'submit' value = 'Submit' />" +
+                "<input type = 'submit' name = 'submitType' value = 'Table' />" +
+                "<input type = 'submit' name = 'submitType' value = 'Ordered List' />" +
                 "</form>";
 
     }
-    @PostMapping("/table")
+    @PostMapping("/display")
     @ResponseBody
-    public String helloPost(@RequestParam String name, @RequestParam String lang1, @RequestParam String lang2, @RequestParam String lang3) {
-//        return "<h1>" + name + "</h1>" +
-//                "<ol><li>" + lang1 + "</li>" +
-//                "<li>" + lang2 + "</li>" +
-//                "<li>" + lang3 + "</li></ol>";
-        return "<h1>" + name + "'s Favorite Languages</h1>" +
-                "<table>" +
-                    "<tr>" +
-                        "<th>#1 Language</th>" +
-                        "<th>#2 Language</th>" +
-                        "<th>#3 Language</th>" +
-                    "</tr>" +
-                    "<tr>" +
-                    "<th>" + lang1 + "</th>" +
-                    "<th>" + lang2 + "</th>" +
-                    "<th>" + lang3 + "</th>" +
-                    "</tr>" +
-                "</table>";
+    public String helloPost(@RequestParam String name,
+                            @RequestParam String lang1,
+                            @RequestParam String lang2,
+                            @RequestParam String lang3,
+                            @RequestParam String submitType) {
+
+        if ("Table".equals(submitType)) {
+            return "<h1>" + name + "'s Favorite Languages</h1>" +
+                        "<table style='border: 1px solid black; border-collapse: collapse;'>" +
+                            "<tr>" +
+                                "<th style='border: 1px solid black; padding: 8px;'>#1 Language</th>" +
+                                "<th style='border: 1px solid black; padding: 8px;'>#2 Language</th>" +
+                                "<th style='border: 1px solid black; padding: 8px;'>#3 Language</th>" +
+                            "</tr>" +
+                            "<tr>" +
+                                "<td style='border: 1px solid black; padding: 8px;'>" + lang1 + "</th>" +
+                                "<td style='border: 1px solid black; padding: 8px;'>" + lang2 + "</th>" +
+                                "<td style='border: 1px solid black; padding: 8px;'>" + lang3 + "</th>" +
+                            "</tr>" +
+                        "</table>";
+        } else if ("Ordered List".equals(submitType)) {
+            return "<h1>" + name + "'s Favorite Languages</h1>" +
+                        "<ol>" +
+                            "<li>" + lang1 + "</li>" +
+                            "<li>" + lang2 + "</li>" +
+                            "<li>" + lang3 + "</li>" +
+                        "</ol>";
+        } else {
+            return "<h1>Error</h1>";
+        }
 
 
     }
